8c3c8702b5a20f1b33c7c8394a318198e6e9918f
David Blackwelder <dblackw2@gmail.com>
2019-01-04 09:41:28 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Hello World'
+    },
+    methods: {
+        changeTitle: function(event) {
+            this.title = event.target.value;
+        }
+    }
+});
